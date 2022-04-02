8eee68f7d3288e25c253c16837ff81831fc04245
Geshii <git@geshii.moe>
2022-04-02 21:10:26 +0200

    init repo

---
+++ app/build/generated/source/buildConfig/release/cafe/miruku/trol/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cafe.miruku.trol;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "cafe.miruku.trol";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
---
+++ app/src/main/java/cafe/miruku/trol/DelegationService.java
@@ -0,0 +1,14 @@
+package cafe.miruku.trol;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
